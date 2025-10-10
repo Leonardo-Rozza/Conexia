65240d7b4c43332a5e6ff9dd43859e30c9c60d8d
Leonardo-Rozza <leonardo155964@gmail.com>
2025-10-10 15:29:29 -0300

    Empezando con la creacion del proyecto con sus dependencias

---
+++ conexia-backend/src/main/java/com/conexia/ConexiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.conexia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexiaBackendApplication.class, args);
+	}
+
+}
