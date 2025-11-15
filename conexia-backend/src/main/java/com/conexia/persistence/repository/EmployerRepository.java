358a366ec6e20a432b035a72ce34ce90bc9ebd89
Leonardo-Rozza <leonardo155964@gmail.com>
2025-11-15 10:22:10 -0300

    Se agregó al EmployerReposiory, la validacion por emial de JPA

--- conexia-backend/src/main/java/com/conexia/persistence/repository/EmployerRepository.java
+++ conexia-backend/src/main/java/com/conexia/persistence/repository/EmployerRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployerRepository extends JpaRepository<EmployerEntity, Long> {
+    boolean existsByEmail(String email);
 }
