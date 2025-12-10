67e52d832878b9a40c7d97b85e6ad0a126182384
Fatima Portillo <fatimarocioportill@gmail.com>
2025-12-09 21:29:11 -0300

    Resolución de conflictos tras merge con frontend

--- src/pages/Egresados.jsx
+++ src/pages/Egresados.jsx
@@ -1,1 +1,26 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Tabs from "./components/Tabs";
 
+import JobList from "./components/Jobs/JobList";
+import CourseList from "./components/Courses/CourseList";
+import ApplicationList from "./components/Applications/ApplicationList";
+import ProfileView from "./components/Profile/ProfileView";
+
+export default function Egresados() {
+  const [tab, setTab] = useState("empleos");
+
+  return (
+    <div className="max-w-6xl mx-auto p-6">
+      <Header />
+      <Tabs tab={tab} setTab={setTab} />
+
+      <div className="mt-8">
+        {tab === "empleos" && <JobList />}
+        {tab === "cursos" && <CourseList />}
+        {tab === "aplicaciones" && <ApplicationList />}
+        {tab === "perfil" && <ProfileView />}
+      </div>
+    </div>
+  );
+}
